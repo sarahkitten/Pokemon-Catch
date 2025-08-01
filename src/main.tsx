73b7af7d089e17004518e507570ca5efd09a8b0e
Sarah Kitten <sarahnkitten@gmail.com>
2025-08-01 12:22:58 -0700

    fix: Title hover animations

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "nes.css/css/nes.min.css"
 import './index.css'
+import './styles/shared.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
